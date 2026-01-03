fa0d4760e69b4c18a4d8ad75ac7c48841746a5e7
Disha3031 <dishak1430@gmail.com>
2026-01-03 14:44:40 +0530

    Add and update backend files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,10 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db";
-import authRouter from "./routes/auth";
-
+import { connectDB } from './config/db';
+import dotenv from 'dotenv';
 dotenv.config();
+import app from './app';
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-// app.use((req, res, next) => {
-//   const start = Date.now();
-//   res.on("finish", () => {
-//     const duration = Date.now() - start;
-//     const method = req.method;
-//     const url = req.originalUrl;
-//     const status = res.statusCode;
-//     const ip = req.ip;
-//     console.log(`[${new Date().toISOString()}] ${method} ${url} ${status} ${duration}ms ip=${ip}`);
-//   });
-//   next();
-// });
-
 connectDB();
-
-app.get("/", (res: Response) => {
-  res.json({ message: "Backend Server is running", success: true });
-});
-
-app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
